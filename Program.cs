6cc468b2fc8db39b74facf3be11abfe1ef94beb5
Alana Santos de Oliveira <asoliveira@fapex.org.br>
2022-07-15 11:24:25 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Contador contador = new Contador();
+            int i = 0;
+            while(i != 4){
+                i = Convert.ToInt32(Console.ReadLine()); //converte p/int
+                switch (i)
+                {
+                    case 1:
+                        contador.zerar();
+                        break;
+                    case 2:
+                        contador.incrementar();
+                        break;
+                    case 3:
+                        Console.WriteLine(contador.retorno());
+                        break;
+                
+                }
+            }
+
+        }
+    }
+
+    class Contador
+    {
+        int numero = 0;
+        public void zerar ()
+        {
+            numero = 0;
+        }
+        public void incrementar ()
+        {
+            numero++;
+        }
+        public int retorno()
+        {
+            return numero;
+        }
+
+
+    }
+}
